bd7d3c72b8589244bc0d56e1700450f782180516
Lucas Domingues <lucas.h.domingues@hotmail.com>
2021-04-08 20:15:36 -0300

    Add project

---
+++ README.md
@@ -0,0 +1,27 @@
+# beholder
+"It observes everything"
+Just another sample project focused on observability on .NET with Elastic stack.
+
+
+## Getting started
+
+On project root folder, run:
+```sh
+docker-compose up
+``` 
+
+The followings urls will be available:
+
+- Kibana http://localhost:5601/
+- Beholder App Sample - https://localhost:5001/swagger/index.html
+
+Try out the swagger enpoint execution and see the metrics on kibana.
+
+
+## Useful links
+
+https://www.elastic.co/guide/en/apm/agent/dotnet/current/config-reporter.html
+
+https://devblogs.microsoft.com/aspnet/monitoring-and-observability-in-cloud-native-asp-net-core-apps/
+
+https://andrewlock.net/writing-logs-to-elasticsearch-with-fluentd-using-serilog-in-asp-net-core/
---
+++ src/Controllers/WeatherForecastController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+namespace Beholder.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            _logger.LogInformation("Getting weather information");
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpGet("/error")]
+        public IEnumerable<WeatherForecast> Error()
+        {
+            throw new Exception("Fake exception");
+        }
+
+        [HttpGet("/delayed")]
+        public IActionResult Delayed()
+        {
+            _logger.LogInformation("Executing task...");
+            Thread.Sleep(new Random().Next(2, 10) * 1000);
+            _logger.LogInformation("Task executed sucessfully");
+            return Ok();
+        }
+    }
+}
